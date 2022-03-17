ef56316c47ca37fca71a89a9636f0a6e1c0d7869
kayleekim409 <81453056+kayleekim409@users.noreply.github.com>
2022-03-16 19:03:52 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,24 +1,27 @@
 function setup() {
-    canvas = createCanvas(200, 200);
+    canvas = createCanvas(300, 300);
     canvas.center();
     video = createCapture(VIDEO);
     video.hide();
 
-    classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/H_MrtorjU/', modelLoaded);
+    classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/H_MrtorjU/model.json', modelLoaded);
 }
+
 function modelLoaded() {
     console.log('Model Loaded!');
 }
+
 function draw() {
     image(video, 0, 0, 300, 300);
     classifier.classify(video, gotResult);
 }
+
 function gotResult(error, results) {
-    if(error) {
+    if (error) {
         console.error(error);
     } else {
         console.log(results);
-        document.getElementById("result_object_name").innerHTML = results[0].label;
-        document.getElementById("result_object_accuracy").innerHTML = results[0].confidence.toFixed(3);
+        document.getElementById("name").innerHTML = results[0].label;
+        document.getElementById("accuracy").innerHTML = results[0].confidence.toFixed(3);
     }
 }
