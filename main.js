067bf8a19907cc39763d23b5cc842f8d76c6f64e
kayleekim409 <81453056+kayleekim409@users.noreply.github.com>
2022-03-22 21:16:43 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,19 +1,20 @@
-function setup() {
-    canvas = createCanvas(300, 300);
-    canvas.center();
-    video = createCapture(VIDEO);
-    video.hide();
-
-    classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/H_MrtorjU/model.json', modelLoaded);
-}
+Webcam.set({
+    width: 300,
+    height: 350,
+    image_format: 'jpeg',
+    image_quality: 90
+});
+var camera = document.getElementById("camera");
+Webcam.attach(camera);
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/H_MrtorjU/model.json', modelLoaded);
 
 function modelLoaded() {
     console.log('Model Loaded!');
 }
 
-function draw() {
-    image(video, 0, 0, 300, 300);
-    classifier.classify(video, gotResult);
+function check() {
+    img = document.getElementById('captured_image');
+    classifier.classify(img, gotResult);
 }
 
 function gotResult(error, results) {
@@ -24,4 +25,10 @@ function gotResult(error, results) {
         document.getElementById("name").innerHTML = results[0].label;
         document.getElementById("accuracy").innerHTML = results[0].confidence.toFixed(3);
     }
+}
+
+function take_snapshot() {
+    Webcam.snap(function(data_uri) {
+        document.getElementById("result").innerHTML = '<img src="' + data_uri + '" id = "captured_image">';
+    });
 }
